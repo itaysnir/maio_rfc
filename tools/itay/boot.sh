99e57d96d3937819571d0b0dce8608caa0aaf9db
itay <itaysnir3687@gmail.com>
2022-08-30 16:50:19 +0300

    abit easier debugging

--- tools/itay/boot.sh
+++ tools/itay/boot.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-ARCH="i386"
+ARCH="x86_64"
+YOCTO_IMAGE="core-image-minimal-qemux86.ext4"
 
 
 qemu-system-"$ARCH" \
-	-kernel arch/"$ARCH"/boot/bzImage \
 	-nographic \
-	-append "console=ttyS0 nokaslr" \
-	-initrd ramdisk.img \
-	-m 512 \
-	--enable-kvm \
-	-cpu host \
-	-s -S &
+	-kernel ../../arch/"$ARCH"/boot/bzImage \
+	-drive file="$YOCTO_IMAGE",if=virtio,format=raw \
+	-append "root=/dev/vda loglevel=15 console=hvc0 nokaslr" \
+	-m 1024 \
+	-s 
+
